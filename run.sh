ad3962edca120a12578caaaad5ca39af76bd056e
Coteh <3276350+Coteh@users.noreply.github.com>
2021-12-01 19:19:41 -0500

    Upgrade shell script to pipe the input file

--- run.sh
+++ run.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-./node_modules/.bin/ts-node "$1/$1.ts"
+if [ "$#" == 0 ]; then
+    >&2 echo "usage: ./run.sh <day> [|test]"
+    exit 1
+fi
+
+INPUT_FILENAME="input.txt"
+
+if [ "$2" == "test" ]; then
+    INPUT_FILENAME="input_test.txt"
+fi
+
+if [ ! -f "$1/$INPUT_FILENAME" ]; then
+    >&2 echo "$1/$INPUT_FILENAME missing: Please provide your input file"
+    exit 1
+fi
+
+cat "$1/$INPUT_FILENAME" | ./node_modules/.bin/ts-node "$1/$1.ts"
