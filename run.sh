a7d283ef47811208399886561f97f4fc6328cca0
James Cote <3276350+Coteh@users.noreply.github.com>
2025-12-14 01:29:54 -0500

    Move all input/output files into a separate submodule
    AoC author doesn't want the input files or puzzle text (sample inputs are part of the puzzle text) published.
    
    https://adventofcode.com/2025/about
    
    Used the gitmodule approach mentioned in this blog post to achieve this:
    https://vadimmi.substack.com/p/using-private-submodule-in-github

--- run.sh
+++ run.sh
@@ -5,15 +5,16 @@ if [ "$#" = 0 ]; then
     exit 1
 fi
 
+DATA_DIR="./data/2021"
 INPUT_FILENAME="input"
 
 if [ "$2" = "test" ]; then
     INPUT_FILENAME="input_test$3"
 fi
 
-if [ ! -f "$1/$INPUT_FILENAME" ]; then
-    >&2 echo "$1/$INPUT_FILENAME missing: Please provide your input file"
+if [ ! -f "${DATA_DIR}/$1/$INPUT_FILENAME" ]; then
+    >&2 echo "${DATA_DIR}/$1/$INPUT_FILENAME missing: Please provide your input file"
     exit 1
 fi
 
-cat "$1/$INPUT_FILENAME" | ./node_modules/.bin/ts-node "$1/$1.ts"
+cat "${DATA_DIR}/$1/$INPUT_FILENAME" | ./node_modules/.bin/ts-node "$1/$1.ts"
--- test.sh
+++ test.sh
@@ -2,19 +2,21 @@
 
 max=25
 
+DATA_DIR="./data/2021"
+
 for i in `seq 1 $max`; do
     val=`printf %02d $i`
     if [ -d "$val" ]; then
         echo "Testing Day $val test"
-        ./run.sh "$val" test | diff "$val/expected_test" -
+        ./run.sh "$val" test | diff "${DATA_DIR}/$val/expected_test" -
         if [ "$?" != 0 ]; then
             >&2 echo "Day $val test file failed"
             exit 1
         fi
         for t in `seq 1 5`; do
-            if [ -f "$val/expected_test$t" ]; then
+            if [ -f "${DATA_DIR}/$val/expected_test$t" ]; then
                 echo "Testing Day $val test $t"
-                ./run.sh "$val" test $t | diff "$val/expected_test$t" -
+                ./run.sh "$val" test $t | diff "${DATA_DIR}/$val/expected_test$t" -
                 if [ "$?" != 0 ]; then
                     >&2 echo "Day $val test file $t failed"
                     exit 1
@@ -22,7 +24,7 @@ for i in `seq 1 $max`; do
             fi
         done
         echo "Testing Day $val real"
-        ./run.sh "$val" | diff "$val/expected" -
+        ./run.sh "$val" | diff "${DATA_DIR}/$val/expected" -
         if [ "$?" != 0 ]; then
             >&2 echo "Day $val failed"
             exit 1
